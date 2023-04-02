52721bbeddf836ddbb522a10f8b2200942a2549f
kaigoerg99 <kaigorg@gmail.com>
2023-04-02 00:16:06 -0400

    Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import {Routes, Route} from "react-router";
-import Navbar from './Navbar';
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home';
 
 function App() {
   // A6: https://docs.google.com/document/d/1wo6HHIQiSqf93vGwBxqHz0Cc_lpohzGEVvt6a4e2xpo/edit
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Navbar/>
         <Routes>
-          {/* <Route index element={}/> */}
+          <Route index element={<Home/>}/>
         </Routes>
         
       </div>
