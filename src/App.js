476b560b688d153655e0683163a37e5a67aca24f
kaigoerg99 <kaigorg@gmail.com>
2023-04-13 18:52:13 -0400

    new folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import {Routes, Route} from "react-router";
-import Navbar from './Navbar/Navbar';
-import Home from './Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
-  // A6: https://docs.google.com/document/d/1wo6HHIQiSqf93vGwBxqHz0Cc_lpohzGEVvt6a4e2xpo/edit
   return (
     <BrowserRouter>
       <div className="App">
