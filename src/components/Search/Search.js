17a49fe19ed95fdf210d19974dd5d0f4d0c5c18c
kaigoerg99 <kaigorg@gmail.com>
2023-04-15 19:40:58 -0400

    rearranged folders

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { searchAPI } from "../API/service";
+import { searchAPI } from "../../imdb/service";
 
 const Search = () => {
     const { searchTerm } = useParams();
