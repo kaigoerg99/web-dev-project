4ed3eb40bf91d542075c1d020177c95d6169eeda
kaigoerg99 <kaigorg@gmail.com>
2023-04-18 20:49:06 -0400

    like movie

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { searchMovie } from "../../imdb/service";
+import { likeMovie, searchMovie } from "../../imdb/service";
+import { useSelector } from "react-redux";
 
 const Search = () => {
     const { searchTerm } = useParams();
     const navigate = useNavigate();
+    const { currentUser } = useSelector((state) => state.users);
     const [results, setResults] = useState([]);
 
     useEffect(() => {
@@ -21,6 +23,14 @@ const Search = () => {
         }
     }
 
+    const onLike = (result) => {
+        if (currentUser) {
+            likeMovie({name: result.title, movieId: result.id});
+        } else {
+            navigate('/login');
+        }
+    }
+
     return (
         <>
             <p className="lead">Search results for {searchTerm}</p>
@@ -34,6 +44,8 @@ const Search = () => {
                                     <h5 className="card-title">{result.title}</h5>
                                     <p className="card-text">{result.description}</p>
                                     <Link className="card-link" to={`/details/${result.id}`}>View details</Link>
+                                    <br></br>
+                                    <i className="bi bi-heart" onClick={() => onLike(result)}></i>
                                 </div>
                             </div>
                         </div>
--- src/imdb/service.js
+++ src/imdb/service.js
@@ -1,31 +1,41 @@
 import axios from "axios";
-export const API = 'https://imdb-api.com/en/API';
+export const IMDB_API = 'https://imdb-api.com/en/API';
+export const MOVIE_API = "http://localhost:4000/api/movies";
 export const API_KEY = 'k_ssytjabd';
+
+const api = axios.create({
+    withCredentials: true,
+});
 
 export const searchMovie = async (searchTerm) => {
     const res = await axios.get(
-        `${API}/SearchMovie/${API_KEY}/${searchTerm}`
+        `${IMDB_API}/SearchMovie/${API_KEY}/${searchTerm}`
     );
     return res;
 };
 
 export const getMovie = async (id) => {
     const res = await axios.get(
-        `${API}/Title/${API_KEY}/${id}`
+        `${IMDB_API}/Title/${API_KEY}/${id}`
     );
     return res;
 }
 
 export const popularMoviesAPI = async () => {
     const res = await axios.get(
-        `${API}/MostPopularMovies/${API_KEY}`
+        `${IMDB_API}/MostPopularMovies/${API_KEY}`
     );
     return res;
 }
 
 export const inTheatersAPI = async () => {
     const res = await axios.get(
-        `${API}/InTheaters/${API_KEY}`
+        `${IMDB_API}/InTheaters/${API_KEY}`
     );
     return res;
 }
+
+export const likeMovie = async (movie) => {
+    const response = await api.post(`${MOVIE_API}/${movie.movieId}/likes`, movie);
+    return response.data;
+};
