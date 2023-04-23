8a9858a475ddaa5329885e23d59a2bdf303e6f61
romanreed <dblrbk@gmail.com>
2023-04-23 17:33:34 -0400

    show liked movies for critics

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,6 +14,9 @@ const Home = () => {
     const [likedMovies, setLikedMovies] = useState([]);
     const {likes} = useSelector((state) => state.likes);
     const { currentUser } = useSelector((state) => state.users);
+    useEffect(() => {
+
+    }, []);
 
     useEffect(() => {
         if (currentUser && likes.length > 0 && currentUser.role === 'viewer') {
@@ -69,7 +72,7 @@ const Home = () => {
     return (
         <>
             {
-                currentUser && likes.length > 0 && currentUser.role === 'viewer' && <>
+                currentUser && likes.length > 0 && (currentUser.role === 'viewer' || currentUser.role === 'critic') && <>
                 <h2>Liked Movies</h2>
                 <div className="table-responsive">
                     <table className="table">
