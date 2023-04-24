0ce8e7b592b4b02f697097962366570627442d54
Kai G <kaigorg@gmail.com>
2023-04-24 13:00:09 -0400

    added keys

--- src/imdb/service.js
+++ src/imdb/service.js
@@ -3,6 +3,14 @@ export const IMDB_API = 'https://imdb-api.com/en/API';
 export const MOVIE_API = "http://localhost:4000/api/movies";
 export const API_KEY = 'k_6eqvvwbo';
 
+/* 
+API Keys:
+k_3cnqqgzp
+k_3ugpx59m
+k_6eqvvwbo
+k_ssytjabd
+*/
+
 export const searchMovie = async (searchTerm) => {
     const res = await axios.get(
         `${IMDB_API}/SearchMovie/${API_KEY}/${searchTerm}`
